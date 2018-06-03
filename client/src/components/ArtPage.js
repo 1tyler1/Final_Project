bfa5f95d8b29c868415330b474b6d3e0a507cf50
1tyler1 <mya12ga@yahoo.com>
2018-06-03 16:59:40 -0400

    updating final project

--- client/src/components/ArtPage.js
+++ client/src/components/ArtPage.js
@@ -5,6 +5,18 @@ class ArtPage extends Component {
     render() {
         return (
             <div>
+            <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo right">Logo</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><Link to={`/users/${this.props.match.params.userId}/edit`}>
+                    Edit
+                </Link></li>
+      </ul>
+    </div>
+  </nav>
                artpage
                <div class="center">
                 <Link class="teal lighten-3 waves-effect waves-light btn center-align btn-small center" to={`/users/${this.props.match.params.userId}/edit`}>
